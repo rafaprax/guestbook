ef445e37a2e568c1fd6850c60f6f3a460dfedc66
Diogo Peixoto <peixotoo@gmail.com>
2015-12-06 13:26:08 -0300

    Add all methods from -LocalServiceImpl to -ServiceImpl

--- portlets/guestbook-portlet/src/main/java/com/liferay/docs/guestbook/service/impl/EntryServiceImpl.java
+++ portlets/guestbook-portlet/src/main/java/com/liferay/docs/guestbook/service/impl/EntryServiceImpl.java
@@ -1,25 +1,52 @@
+
 package com.liferay.docs.guestbook.service.impl;
 
+import com.liferay.docs.guestbook.model.Entry;
 import com.liferay.docs.guestbook.service.base.EntryServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.service.ServiceContext;
+
+import java.util.List;
 
 /**
- * The implementation of the entry remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.docs.guestbook.service.EntryService} interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
+ * The implementation of the entry remote service. <p> All custom service
+ * methods should be put in this class. Whenever methods are added, rerun
+ * ServiceBuilder to copy their definitions into the
+ * {@link com.liferay.docs.guestbook.service.EntryService} interface. <p> This
+ * is a remote service. Methods of this service are expected to have security
+ * checks based on the propagated JAAS credentials because this service can be
+ * accessed remotely. </p>
  *
  * @author Diogo Peixoto
  * @see com.liferay.docs.guestbook.service.base.EntryServiceBaseImpl
  * @see com.liferay.docs.guestbook.service.EntryServiceUtil
  */
 public class EntryServiceImpl extends EntryServiceBaseImpl {
-    /*
-     * NOTE FOR DEVELOPERS:
-     *
-     * Never reference this interface directly. Always use {@link com.liferay.docs.guestbook.service.EntryServiceUtil} to access the entry remote service.
-     */
+
+	/*
+	 * NOTE FOR DEVELOPERS: Never reference this interface directly. Always use
+	 * {@link com.liferay.docs.guestbook.service.EntryServiceUtil} to access the
+	 * entry remote service.
+	 */
+
+	public Entry add(Entry entry, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		return entryLocalService.add(entry, serviceContext);
+	}
+
+	public int countByGroupIdGuestbookId(long groupId, long guestbookId)
+		throws SystemException {
+
+		return entryLocalService.countByGroupIdGuestbookId(groupId, guestbookId);
+	}
+
+	public List<Entry> findByGroupIdGuestbookId(
+		long groupId, long guestbookId, int start, int end)
+		throws SystemException {
+
+		return entryLocalService.findByGroupIdGuestbookId(
+			groupId, guestbookId, start, end);
+	}
 }
--- portlets/guestbook-portlet/src/main/java/com/liferay/docs/guestbook/service/impl/GuestbookServiceImpl.java
+++ portlets/guestbook-portlet/src/main/java/com/liferay/docs/guestbook/service/impl/GuestbookServiceImpl.java
@@ -1,25 +1,50 @@
+
 package com.liferay.docs.guestbook.service.impl;
 
+import com.liferay.docs.guestbook.model.Guestbook;
 import com.liferay.docs.guestbook.service.base.GuestbookServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.service.ServiceContext;
+
+import java.util.List;
 
 /**
- * The implementation of the guestbook remote service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.docs.guestbook.service.GuestbookService} interface.
- *
- * <p>
- * This is a remote service. Methods of this service are expected to have security checks based on the propagated JAAS credentials because this service can be accessed remotely.
- * </p>
+ * The implementation of the guestbook remote service. <p> All custom service
+ * methods should be put in this class. Whenever methods are added, rerun
+ * ServiceBuilder to copy their definitions into the
+ * {@link com.liferay.docs.guestbook.service.GuestbookService} interface. <p>
+ * This is a remote service. Methods of this service are expected to have
+ * security checks based on the propagated JAAS credentials because this service
+ * can be accessed remotely. </p>
  *
  * @author Diogo Peixoto
  * @see com.liferay.docs.guestbook.service.base.GuestbookServiceBaseImpl
  * @see com.liferay.docs.guestbook.service.GuestbookServiceUtil
  */
 public class GuestbookServiceImpl extends GuestbookServiceBaseImpl {
-    /*
-     * NOTE FOR DEVELOPERS:
-     *
-     * Never reference this interface directly. Always use {@link com.liferay.docs.guestbook.service.GuestbookServiceUtil} to access the guestbook remote service.
-     */
+
+	/*
+	 * NOTE FOR DEVELOPERS: Never reference this interface directly. Always use
+	 * {@link com.liferay.docs.guestbook.service.GuestbookServiceUtil} to access
+	 * the guestbook remote service.
+	 */
+
+	public Guestbook add(Guestbook guestbook, ServiceContext serviceContext)
+		throws PortalException, SystemException {
+
+		return guestbookLocalService.add(guestbook, serviceContext);
+	}
+
+	public List<Guestbook> findByGroupId(long groupId)
+		throws SystemException {
+
+		return guestbookLocalService.findByGroupId(groupId);
+	}
+
+	public Guestbook findByPrimaryKey(long guestbookId)
+		throws SystemException {
+
+		return guestbookPersistence.fetchByPrimaryKey(guestbookId);
+	}
 }
