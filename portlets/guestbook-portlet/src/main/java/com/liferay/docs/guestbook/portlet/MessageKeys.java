1921dbbe37d8a27cb06e476c921cbca7cf7f68ce
Diogo Peixoto <peixotoo@gmail.com>
2015-12-08 16:39:41 -0300

    Add new message keys

--- portlets/guestbook-portlet/src/main/java/com/liferay/docs/guestbook/portlet/MessageKeys.java
+++ portlets/guestbook-portlet/src/main/java/com/liferay/docs/guestbook/portlet/MessageKeys.java
@@ -3,6 +3,11 @@ package com.liferay.docs.guestbook.portlet;
 
 public class MessageKeys {
 
+	//Entry
 	public static final String ENTRY_ADDED = "entry-added";
+	
+	//Guestbook
 	public static final String GUESTBOOK_ADDED = "guestbook-added";
+	public static final String GUESTBOOK_DELETED = "guestbook-deleted";
+	public static final String GUESTBOOK_UPDATED = "guestbook-updated";
 }
