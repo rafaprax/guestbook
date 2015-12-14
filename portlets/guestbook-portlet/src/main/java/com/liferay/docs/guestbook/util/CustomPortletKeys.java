47e9c50c939b59f577b422d9aac6e449e16475cd
Diogo Peixoto <peixotoo@gmail.com>
2015-12-14 18:17:39 -0300

    Refactor entryIndexer to call CustomPortletKeys

---
+++ portlets/guestbook-portlet/src/main/java/com/liferay/docs/guestbook/util/CustomPortletKeys.java
@@ -0,0 +1,11 @@
+
+package com.liferay.docs.guestbook.util;
+
+import com.liferay.portal.util.PortletKeys;
+
+public class CustomPortletKeys extends PortletKeys {
+
+	public static final String GUESTBOOK = "guestbook_WAR_guestbookportlet";
+	public static final String GUESTBOOK_ADMIN =
+		"guestbookadmin_WAR_guestbookportlet";
+}
